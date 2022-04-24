9540b09b2c8c9f09422c96eaaa0dbfc9685e130f
yasira <yasirayusif@gmail.com>
2022-04-24 15:46:13 -0700

    change from the eval() to function object

--- Js/calculator.js
+++ Js/calculator.js
@@ -6,10 +6,10 @@ function OutPut (num) {
     
     inputField.value += num;
 
-    enableBtn() //calling the enable function
+    enableBtn() // invoking remove disabled button  
 }
 
-// enable disabled button function 
+// remove disabled button function 
 function enableBtn() {
 
     const btn = document.getElementById('Btn')
@@ -21,12 +21,18 @@ function enableBtn() {
 // equall to function
 function equalTo() {
 
-    try{
+    try {
+        // if the input field is gempty 
         if (inputField.value === "") { 
             return
         }
+        // eval is slow and dangerous to use 
+        // return inputField.value = eval(inputField.value); // eval does the calculation and gives us the result when we click on the submit botton
+
+        // new Function is a function which create a new instance of the same function 
+        const newOutputValue = new Function ('return ' + inputField.value);
         
-        return inputField.value = eval(inputField.value); // eval does the calculation and gives us the result when we click on the submit botton
+        return inputField.value = newOutputValue();
     }
     catch (error) {
         alert('incorrect number')
@@ -45,5 +51,5 @@ function Clear() {
 // function for deleting the values one by one 
 function Del() {
     
-    inputField.value = inputField.value.slice(0, -1); //using the slice methodto delete it one by one
+    inputField.value = inputField.value.slice(0, -1); //using the slice method to delete it one by one
 }
